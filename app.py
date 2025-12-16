b87e850dfdb2cf0acdfd3401c94d1be9f394c264
William Lu <32563160+williaml12@users.noreply.github.com>
2025-12-16 13:33:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ with col1:
 
 with col2:
     st.markdown("<br>", unsafe_allow_html=True)
-    st.button("⇄ Swap", on_click=swap_currencies)
+    st.button("⇄", on_click=swap_currencies)
 
 with col3:
     to_currency = st.selectbox(
