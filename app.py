dbbe884664476b415656b41098b23197c5157a59
William Lu <32563160+williaml12@users.noreply.github.com>
2025-12-19 13:51:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import plotly.express as px
 
 # ================== CONFIG ==================
-API_KEY = "cur_live_fu5Qwj51XDciBICSWKky2Zgjzf3DSa52yj6pqj4C"
+# API_KEY = "cur_live_fu5Qwj51XDciBICSWKky2Zgjzf3DSa52yj6pqj4C"
+CURRENCY_API_KEY = "cur_live_fu5Qwj51XDciBICSWKky2Zgjzf3DSa52yj6pqj4C"
 
 st.set_page_config(page_title="Real-Time Currency Converter", page_icon="💱")
 
